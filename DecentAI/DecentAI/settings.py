59dc6337d642a0463f4720f127f627951f5fc176
Sayviour <dylan.j.sayre@gmail.com>
2024-05-17 13:17:47 -0400

    adding env

--- DecentAI/DecentAI/settings.py
+++ DecentAI/DecentAI/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!6o0gjrqb%q4b@!wfvbtom12%5o6(nbsv0#=bnxzr7ew+ers%$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
